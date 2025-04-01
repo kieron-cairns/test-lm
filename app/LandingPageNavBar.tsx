3219e38804f3570260907478b5691460c992885d
kieron-cairns <kieroncairns@protonmail.com>
2025-04-01 11:23:40 +0100

    CHANGE: video play es lint error

--- app/LandingPageNavBar.tsx
+++ app/LandingPageNavBar.tsx
@@ -1,17 +1,16 @@
-
 import React, { useEffect, useRef } from "react";
 import Link from "next/link";
 import "./LandingNavBar.css";
 
 const LandingNavBar = () => {
-  const videoRef = useRef(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null); // Specify the correct type for the ref
 
   useEffect(() => {
     const delay = 2000; // Delay in milliseconds (e.g., 2000ms = 2 seconds)
 
     const timer = setTimeout(() => {
       if (videoRef.current) {
-        videoRef.current.play();
+        videoRef.current.play(); // Now TypeScript knows videoRef.current is a video element
       }
     }, delay);
 
@@ -20,16 +19,16 @@ const LandingNavBar = () => {
 
   return (
     <nav className="flex items-center justify-between py-9 px-10 bg-white">
-         <div className="video-container">
-      <video 
-        ref={videoRef}
-        src="/Luxe-Meadow-Black-No-Writing-Animate-MP4.mp4" 
-        muted 
-        playsInline 
-        preload="auto" 
-        width={100} 
-        height={200} 
-      />
+      <div className="video-container">
+        <video
+          ref={videoRef}
+          src="/Luxe-Meadow-Black-No-Writing-Animate-MP4.mp4"
+          muted
+          playsInline
+          preload="auto"
+          width={100}
+          height={200}
+        />
       </div>
 
       <h1 className="nav-heading">Luxe Meadow</h1>
