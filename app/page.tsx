299e83b85aacb2e6a13484cdf211fba51c75ceee
kieron-cairns <kieroncairns@protonmail.com>
2025-04-03 15:37:51 +0100

    CHANGE:

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function LandingPage() {
     <div>
       <NavBar />
       <div id="main">
-        <video id="box1" autoPlay muted playsInline>
+        <video id="box1" autoPlay muted playsInline webkit-playsinline>
         <source src="/images/Animated-Luxe-Meadow-White-Font_1@4x-No-Writing.mp4" type="video/mp4" />
         <div id="fallback-box1"></div>
       </video>
