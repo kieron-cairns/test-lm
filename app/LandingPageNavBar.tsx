7c75d078c30d1b9340f116c9d1767f98734fd555
kieron-cairns <kieroncairns@protonmail.com>
2025-04-04 15:19:26 +0100

    CHANGE: Landing Nav Logo Vide replaced with GIF

--- app/LandingPageNavBar.tsx
+++ app/LandingPageNavBar.tsx
@@ -1,32 +1,20 @@
-import React, { useEffect, useRef } from "react";
+"use client";
+
+import React from "react";
+import Image from "next/image";
 import "./LandingNavBar.css";
 
 const LandingNavBar = () => {
-  const videoRef = useRef<HTMLVideoElement | null>(null); // Specify the correct type for the ref
-
-  useEffect(() => {
-    const delay = 2000; // Delay in milliseconds (e.g., 2000ms = 2 seconds)
-
-    const timer = setTimeout(() => {
-      if (videoRef.current) {
-        videoRef.current.play(); // Now TypeScript knows videoRef.current is a video element
-      }
-    }, delay);
-
-    return () => clearTimeout(timer); // Cleanup timer on component unmount
-  }, []);
-
   return (
     <nav className="flex items-center justify-between py-9 px-10 bg-white">
       <div className="video-container">
-        <video
-          ref={videoRef}
-          src="/Luxe-Meadow-Black-No-Writing-Animate-MP4.mp4"
-          muted
-          playsInline
-          preload="auto"
+        <Image
+          src="/Luxe-Meadow-Black-No-Writing-Animated.gif"
+          alt="Luxe Meadow Logo"
           width={100}
-          height={200}
+          height={100}
+          priority
+          style={{ objectFit: "contain" }}
         />
       </div>
 
